2c548aff073d5d06078e432caf34e60822759bf0
Baptiste Nouailhac <baptiste.nouailhac@epitech.eu>
2020-07-21 22:35:24 +0200

    add wall and bug correction

--- Assets/script/Battle/Ennemie_Script.cs
+++ Assets/script/Battle/Ennemie_Script.cs
@@ -4,45 +4,57 @@ using UnityEngine;
 
 public class Ennemie_Script : MonoBehaviour
 {
-    public GameObject ennemie1;
-    public GameObject ennemie2;
-    public GameObject ennemie3;
-    public GameObject ennemie4;
-    public GameObject ennemie5;
     int nb;
 
     // Start is called before the first frame update
     void Start()
     {
-        ennemie1 = GameObject.Find("Ennemie1");
         GameObject.Find("NomEnnemie1").GetComponent<TMPro.TextMeshProUGUI>().text = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameData>().battleEnnemie[0];
-        ennemie2 = GameObject.Find("Ennemie2");
-        GameObject.Find("NomEnnemie2").GetComponent<TMPro.TextMeshProUGUI>().text = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameData>().battleEnnemie[1];
-        ennemie3 = GameObject.Find("Ennemie3");
-        GameObject.Find("NomEnnemie3").GetComponent<TMPro.TextMeshProUGUI>().text = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameData>().battleEnnemie[2];
-        ennemie4 = GameObject.Find("Ennemie4");
-        GameObject.Find("NomEnnemie4").GetComponent<TMPro.TextMeshProUGUI>().text = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameData>().battleEnnemie[3];
-        ennemie5 = GameObject.Find("Ennemie5");
-        GameObject.Find("NomEnnemie5").GetComponent<TMPro.TextMeshProUGUI>().text = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameData>().battleEnnemie[4];
+        
+        
+        
         if (GameObject.FindGameObjectWithTag("GameController").GetComponent<GameData>().battleEnnemie[1] == "0")
         {
-            ennemie2.SetActive(false);
+            GameObject.Find("flèche2").SetActive(false);
+            GameObject.Find("NomEnnemie2").SetActive(false);
+            GameObject.Find("Ennemie2").SetActive(false);
             nb++;
+        } else
+        {
+            GameObject.Find("NomEnnemie2").GetComponent<TMPro.TextMeshProUGUI>().text = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameData>().battleEnnemie[1];
         }
         if (GameObject.FindGameObjectWithTag("GameController").GetComponent<GameData>().battleEnnemie[2] == "0")
         {
-            ennemie3.SetActive(false);
+            GameObject.Find("flèche3").SetActive(false);
+            GameObject.Find("NomEnnemie3").SetActive(false);
+            GameObject.Find("Ennemie3").SetActive(false);
             nb++;
+        }
+        else
+        {
+            GameObject.Find("NomEnnemie3").GetComponent<TMPro.TextMeshProUGUI>().text = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameData>().battleEnnemie[2];
         }
         if (GameObject.FindGameObjectWithTag("GameController").GetComponent<GameData>().battleEnnemie[3] == "0")
         {
-            ennemie4.SetActive(false);
+            //GameObject.Find("flèche4").SetActive(false);
+            //GameObject.Find("NomEnnemie4").SetActive(false);
+            GameObject.Find("Ennemie4").SetActive(false);
             nb++;
         }
-        if (GameObject.FindGameObjectWithTag("GameController").GetComponent<GameData>().battleEnnemie[4] == "0")
+        else
         {
-            ennemie5.SetActive(false);
+            GameObject.Find("NomEnnemie4").GetComponent<TMPro.TextMeshProUGUI>().text = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameData>().battleEnnemie[3];
+        }
+        if (GameObject.FindGameObjectWithTag("GameController").GetComponent<GameData>().battleEnnemie[4] == "0")
+        {   
+            GameObject.Find("flèche5").SetActive(false);
+            GameObject.Find("NomEnnemie5").SetActive(false);
+            GameObject.Find("Ennemie5").SetActive(false);
             nb++;
+        }
+        else
+        {
+            GameObject.Find("NomEnnemie5").GetComponent<TMPro.TextMeshProUGUI>().text = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameData>().battleEnnemie[4];
         }
     }
 
--- Assets/script/Game/movement.cs
+++ Assets/script/Game/movement.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     public float speed;
+    public GameObject dialog;
+    public bool down;
+    public bool up;
+    public bool right;
+    public bool left;
 
     // Start is called before the first frame update
-    void Start()
+    void Start()    
     {
+        down = false;
+        up = false;
+        right = false;
+        left = false;
+        dialog = GameObject.Find("dialogue");
+        dialog.SetActive(false);
         if (GameObject.FindGameObjectWithTag("GameController").GetComponent<GameData>().Life <= 1)
         {
             GameObject.Find("life (1)").SetActive(false);
@@ -21,18 +32,72 @@ public class movement : MonoBehaviour
         speed = 0.18f;
     }
 
+    void FixedUpdate()
+    {
+
+    }
+
     // Update is called once per frame
     void Update()
     {
         Vector3 dp = new Vector3();
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(KeyCode.Q) && !left)
         {
+            GameObject.Find("Player").GetComponent<SpriteRenderer>().flipX = true;
             dp.x -= speed;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && !right)
         {
+            GameObject.Find("Player").GetComponent<SpriteRenderer>().flipX = false;
             dp.x += speed;
+        }
+        if (Input.GetKey(KeyCode.S) && !down)
+        {
+            dp.y -= speed;
+        }
+        if (Input.GetKey(KeyCode.Z) && !up)
+        {
+            dp.y += speed;
         }
         transform.position += dp;
     }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "wall right")
+        {
+            right = true;
+        }
+        if (collision.gameObject.tag == "wall up")
+        {
+            up = true;
+        }
+        if (collision.gameObject.tag == "wall left")
+        {
+            left = true;
+        }
+        if (collision.gameObject.tag == "wall down")
+        {
+            down = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "wall right")
+        {
+            right = false;
+        }
+        if (collision.gameObject.tag == "wall up")
+        {
+            up = false;
+        }
+        if (collision.gameObject.tag == "wall left")
+        {
+            left = false;
+        }
+        if (collision.gameObject.tag == "wall down")
+        {
+            down = false;
+        }
+    }
+    
 }
